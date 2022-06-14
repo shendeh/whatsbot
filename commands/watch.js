e1f3363bd78d670357a0d08766066e8123518123
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-14 17:53:50 +0530

    Update watch.js

--- commands/watch.js
+++ commands/watch.js
@@ -16,20 +16,20 @@ async function getDetails(title) {
                     return "error";
                 });
                 let watchdata = data[0];
-                let caption = `Here is the details of the ${watchdata.type} 👇\nTitle: *${watchdata.title}*\n`;
+                let caption = `Here is the details of the ${watchdata.type}\n\nTitle : *${watchdata.title}*\n`;
                 if (watchdata.release_year){
-                    caption += `Released in Year: *${watchdata.release_year}*\n`;
+                    caption += `Released in year : *${watchdata.release_year}*\n`;
                 }
                 if(watchdata.score){
                     if (watchdata.score.imdb){
-                        caption += `IMDB Rating: *${watchdata.score.imdb}*\n`;
+                        caption += `Imdb rating : *${watchdata.score.imdb}*\n`;
                     }
                     if (watchdata.score.tmdb){
-                        caption += `TMDB Rating: *${watchdata.score.tmdb}*\n`;
+                        caption += `Tmdb rating: *${watchdata.score.tmdb}*\n`;
                     }
                 }
                 if (watchdata.providers){
-                    caption += `\n${watchdata.title} ${watchdata.type} is available to watch on:\n`;
+                    caption += `\n${watchdata.title} ${watchdata.type} is available to watch on :\n`;
                     let providers = watchdata.providers;
                     for(let provider in providers) {
                         let providerTitle = provider.charAt(0).toUpperCase() + provider.substring(1).toLowerCase();
@@ -44,7 +44,7 @@ async function getDetails(title) {
                 });
                 return out;
             } else {
-                return "No Results";
+                return "No results";
             }
         })
         .catch(function (error) {
@@ -56,10 +56,10 @@ const execute = async (client,msg,args) => {
     msg.delete(true);
     let data = await getDetails(args.join(' '));
     if (data == "error") {
-        await client.sendMessage(msg.to, `🙇‍♂️ *Error*\n\n` + "```Something Unexpected Happened while fetching Movie/TV Show Details.```");
+        await client.sendMessage(msg.to, `*Error*\n\n` + "```Something unexpected happened while fetching movie or tv show details```");
     }
     else if (data == "No Results") {
-        await client.sendMessage(msg.to, `🙇‍♂️ *No Results Found!*\n\n` + "```Please check the name of Movie/TV Show you have entered.```");
+        await client.sendMessage(msg.to, `*No results found*\n\n` + "```Please check the name of movie or tv show you have entered```");
     }
     else {
         await client.sendMessage(msg.to, new MessageMedia(data.mimetype, data.thumbdata, data.filename), { caption: data.caption });
@@ -69,9 +69,9 @@ const execute = async (client,msg,args) => {
 
 module.exports = {
     name: 'Watch',
-    description: 'Get show/movie details',
+    description: 'Get movie or show details',
     command: '!watch',
     commandType: 'plugin',
     isDependent: false,
-    help: `*Watch*\n\nGet information about where to watch a Movie/Show. \n\n*!watch [movie-name]*\n`,
+    help: `*Watch*\n\nGet information about where to watch a movie or show\n\n*!watch [movie-name]*\n`,
     execute};
