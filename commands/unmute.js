89cbc76cf2e4010c228a32ce8b68beb5d48616c5
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-14 17:49:56 +0530

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,7 +3,7 @@ const execute = async (client,msg) => {
     if (!msg.to.includes("-")) {
         let chat = await msg.getChat();
         await chat.unmute(true);
-        msg.reply(`*🗣 Unmuted*\n\nYou have been unmuted\n\n _Powered by WhatsBot_`);
+        msg.reply(`*Err you have been unmuted`);
     }
 };
 
@@ -13,5 +13,5 @@ module.exports = {
     command: '!unmute', //command with prefix. Ex command: '!test'
     commandType: 'admin', //
     isDependent: true, //whether this command is related/dependent to some other command
-    help: 'This command is related to !mute. Type !help mute to learn about this', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+    help: 'This command is related to !mute ! Type !help mute to learn about this', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
     execute};
