9c3ff1f679964d63673e6d801fe09e4f06dd442f
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-14 17:15:44 +0530

    Update awake.js

--- commands/awake.js
+++ commands/awake.js
@@ -1,12 +1,12 @@
 //jshint esversion:8
 const execute = async (client, msg) => {
   client.sendPresenceAvailable();
-  msg.reply("```" + "I will be online from now." + "```");
+  msg.reply("```" + "I will be online from now" + "```");
 };
 
 module.exports = {
   name: "Awake",
-  description: "Stay online always !",
+  description: "Stay online always",
   command: "!awake",
   commandType: "plugin",
   isDependent: false,
