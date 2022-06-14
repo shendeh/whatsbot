f7f830ae0738c9cd2ef23e4e31538a0e27a4a368
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-14 17:29:45 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -7,16 +7,16 @@ const execute = async (client, msg) => {
     let unmuteDate = new Date();
     unmuteDate.setSeconds(Number(unmuteDate.getSeconds()) + 3600);
     await chat.mute(unmuteDate);
-    msg.reply(`*🤫 Muted*\n\nYou have been muted for 1 hour\n\n _Powered by WhatsBot_`);
+    msg.reply(`Congratulations you have been muted for 1 hour`);
   }
 };
 
 module.exports = {
   name: "Mute", //name of the module
-  description: "mute the current chat", // short description of what this command does
+  description: "Mute the current chat", // short description of what this command does
   command: "!mute", //command with prefix. Ex command: '!test'
   commandType: "admin", //
   isDependent: false, //whether this command is related/dependent to some other command
-  help: `Type !mute in chat to mute the chat for 1 hour. use !unmute to unmute the chat.`, // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+  help: `Type !mute in chat to mute the chat for 1 hour ! Use !unmute to unmute the chat`, // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
   execute,
 };
