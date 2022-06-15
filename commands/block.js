fc9f2e792bbe0b760cecd73aa1ddeaa9d76a265e
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-15 12:54:54 +0530

    Update block.js

--- commands/block.js
+++ commands/block.js
@@ -1,7 +1,7 @@
 //jshint esversion:8
 const execute = async (client, msg) => {
   if (!msg.to.includes("-")) {
-    await msg.reply(`Umm as I have already mentioned above that this is not a right place for you to spam however you ignored that message so I just blocked you\n\nNow you can't do anything until my mistress comes online and unblocks you ! Good bye demon have a great day ahead`);
+    await msg.reply(`Hey I haven't approved you for personal messaging me yet , my mistress will respond when she comes back online if she wants to\n\nPlease don't spam unless you wish to be blocked and reported\n\nUmm as I have already mentioned above that this is not a right place for you to spam however you ignored that message so I just blocked you\n\nNow you can't do anything until my mistress comes online and unblocks you ! Good bye have a great day ahead`);
     let chat = await msg.getChat();
     let contact = await chat.getContact();
     contact.block();
