86b008745eef8aa46e13eb7be277c4dc8d01ac2b
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-16 17:59:56 +0530

    Create image.js

---
+++ commands/image.js
@@ -0,0 +1,22 @@
+//jshint esversion:8
+const {MessageMedia} = require('whatsapp-web.js');
+
+const execute = async (client,msg) => {
+    msg.delete(true);
+    let quotedMsg = await msg.getQuotedMessage();
+    if (quotedMsg.hasMedia) {
+        let attachmentData = await quotedMsg.downloadMedia();
+        await client.sendMessage(msg.to, new MessageMedia(attachmentData.mimetype, attachmentData.data, attachmentData.filename), { sendMediaAsImage: true });
+    } else {
+        await client.sendMessage(msg.to, `*Error*\n\n` + "No sticker found to make a image");
+    }
+};
+
+module.exports = {
+    name: 'Image maker',
+    description: 'Generates image from sticker',
+    command: '!image',
+    commandType: 'plugin',
+    isDependent: false,
+    help: `*Image maker*\n\nCreates image from sticker\n\nReply an sticker with *!image* to get a image of that sticker`,
+    execute};
