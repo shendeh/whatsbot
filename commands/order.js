354f26bd09e9bea2c27f5f2894f65bf147802fb9
Yuki <102640547+Dark-team-11@users.noreply.github.com>
2022-06-17 16:42:41 +0530

    Create order.js

---
+++ commands/order.js
@@ -0,0 +1,17 @@
+//jshint esversion:8
+const { Order } = require("whatsapp-web.js");
+const execute = async (client,msg/*,args*/) => {
+    const chat = await msg.getChat();
+    var O=new Order();
+    O.currency="rupees";
+    await chat.sendMessage(O); 
+};
+
+module.exports = {
+    name: 'Play',
+    description: 'A quick way to play some games',
+    command: '!play',
+    commandType: 'info',
+    isDependent: false,
+    help: '*Play*\n\nType !play and touch any of the buttons on your screen',
+    execute};
